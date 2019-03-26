55139b5c8a4fcb1fecf726729b12698a60362c92
Brandon <bvasquez1994@gmail.com>
2019-03-26 18:31:55 -0400

    Add files via upload

---
+++ NumberWizard.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems; // Handles the button press issue.
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText; 
+
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Starts the game by choosing a random value from the NextGuess() function.
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    // Sets the guessed value as the minimum and increases the value by 1.
+    public void OnPressHigher()
+    {
+        min = guess +1;
+        NextGuess();
+    }
+
+    // Sets the guessed value as the maximum and increases the value by 1.
+    public void OnPressLower()
+    {
+        max = guess -1;
+        NextGuess();
+    }
+
+    // Chooses a random value between the maximum and minimum and sets it to the guess value in the text field.
+    void NextGuess()
+    {
+        guess = Random.Range(min,  max + 1);
+        guessText.text = guess.ToString();
+        // Turns off the highlighted button once the button is clicked.
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
+
---
+++ SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Loads the next scene using the current scene index value.
+    public void LoadNextScene()
+    {
+        // Create a variable to save the current scene index.
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // Have the scene manager load the next game scene by adding one to the current scene index.
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+    // Loads the start scene by passing in the value 0.
+    public void LoadStartScene()
+    {
+        // Load the very first scene in our game. Index 0 is the start game menu scene.
+        SceneManager.LoadScene(0);
+    }
+
+    // Allows the user to close and quit the application using an in game button.
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
